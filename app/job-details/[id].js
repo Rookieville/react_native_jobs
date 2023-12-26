481eedd76f37ff7b081b0d462a4b486326bb098b
Obeng-Danso Richard <rdanso2003@gmail.com>
2023-12-26 14:22:22 +0000

    finish

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -5,6 +5,8 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } fro
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
 const JobDetails = () => {
     const params = useSearchParams();
     const router = useRouter();
@@ -14,8 +16,30 @@ const JobDetails = () => {
     })
 
     const [refreshing, setRefreshing] = useState(false);
+    const [activeTab, setActiveTab] = useState(tabs[0]);
 
     const onRefresh = () => []
+
+    const dispalyTabContent = () => {
+        switch (activeTab) {
+            case "Qualifications":
+                return <Specifics
+                    title="Qualifications"
+                    points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+                />
+            case "About":
+                return <JobAbout 
+                    info={data[0].job_description ?? "No data provided"}
+                />
+            case "Responsibilities":
+                return <Specifics
+                    title="Responsibilities"
+                    points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+                />
+            default:
+                break;
+        }
+    }
 
 
     return (
@@ -55,12 +79,16 @@ const JobDetails = () => {
                                 companyLogo={data[0].employer_logo}
                                 jobTitle={data[0].job_title}
                                 companyName={data[0].employer_name}
-                                Locatio ={data[0].job_country}
-                            
+                                Location ={data[0].job_country}
                             />
+
                             <JobTabs 
-                            
+                                tabs={tabs}
+                                activeTab={activeTab}
+                                setActiveTab={setActiveTab}
                             />
+
+                            {dispalyTabContent()}
                         </View>
                     )}
                 </ScrollView>
